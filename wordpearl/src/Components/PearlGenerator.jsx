5714a6686ea8d69ef4f37352942f334131f1074d
FlashSampson <flashsampson1991@gmail.com>
2022-11-17 16:09:21 +0000

    continued work on pearl generator

--- wordpearl/src/Components/PearlGenerator.jsx
+++ wordpearl/src/Components/PearlGenerator.jsx
@@ -9,34 +9,36 @@ const PearlGenerator = ()=> {
     const [pearl, setPearl] = useState('');
     const [oddLetters, setOddLetters] = useState('')
     const [isSuccessful, setisSuccessful] = useState(false)
+    const [text, setText] = useState('')
 
 
     const handleSubmit = event => {
         event.preventDefault()
-        setPearl(event.target.value);
-        setOddLetters(pearlCheck(pearl))
+        console.log(oddLetters)
         if (oddLetters === []) { 
              setisSuccessful(true);
         }
-        // setisSuccessful(false); 
         setPearl('')
     }
 
-
-
-
+    const handleChange = (event)=>{
+        setText(event.target.value)
+        setPearl(event.target.value);
+        setOddLetters(pearlCheck(pearl))
+        console.log(text)
+    }
 
 return <div className='pearlGenerator'>
     <label className='pearlGenerator'>Create a Pearl !</label>
-    <form onSubmit={handleSubmit}>
+    <form >
         <textarea name="pearl" id="pearl" cols="50" rows="12"
-        value={pearl} onChange={(event)=> setPearl(event.target.value)}></textarea>
+        value={pearl} onChange={handleChange}></textarea>
         <div >
       { //Check if submission pearl
         ( isSuccessful === true ) ? <p> Successful Pearl </p> : <p>Oddletters: {oddLetters} </p>
         }
     </div>
-    <button type='submit'> Check Pearl </button>
+    <button type='submit' onClick={handleSubmit}> Check Pearl </button>
     </form>
 </div>
 }
