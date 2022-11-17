49e246d64e7e60f9170c674163247f3df24aabf8
Michael Taylor <mtaylor7193r@gmail.com>
2022-11-17 15:44:39 +0000

    progress with pearlgen

--- wordpearl/src/Components/PearlGenerator.jsx
+++ wordpearl/src/Components/PearlGenerator.jsx
@@ -4,22 +4,39 @@ import { useState } from 'react';
 import pearlCheck from './PearlCheck'
 
 const PearlGenerator = ()=> {
+
+
     const [pearl, setPearl] = useState('');
     const [oddLetters, setOddLetters] = useState('')
     const [isSuccessful, setisSuccessful] = useState(false)
+
+
     const handleSubmit = event => {
         event.preventDefault()
         setPearl(event.target.value);
         setOddLetters(pearlCheck(pearl))
+        if (oddLetters === []) { 
+             setisSuccessful(true);
+        }
+        // setisSuccessful(false); 
         setPearl('')
     }
+
+
+
+
+
 return <div className='pearlGenerator'>
     <label className='pearlGenerator'>Create a Pearl !</label>
     <form onSubmit={handleSubmit}>
         <textarea name="pearl" id="pearl" cols="50" rows="12"
         value={pearl} onChange={(event)=> setPearl(event.target.value)}></textarea>
-        <p> Odd Letters : {oddLetters}</p>
-    <button disabled={!isSuccessful} type='submit'> Check Pearl </button>
+        <div >
+      { //Check if submission pearl
+        ( isSuccessful === true ) ? <p> Successful Pearl </p> : <p>Oddletters: {oddLetters} </p>
+        }
+    </div>
+    <button type='submit'> Check Pearl </button>
     </form>
 </div>
 }
