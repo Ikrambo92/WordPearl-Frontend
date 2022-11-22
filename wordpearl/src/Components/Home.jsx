9427e77481bf0500f2702d7c208f9f277dd2fcb3
Kantideep Thota <kantideep@gmail.com>
2022-11-22 14:04:00 +0000

    adding pearls to home page

--- wordpearl/src/Components/Home.jsx
+++ wordpearl/src/Components/Home.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
 import './Home.css'
+import { useState } from "react";
+import { useEffect } from "react";
+import { getPearls } from "../Utils/apis";
+import PearlCard from "./PearlCard";
 
 const Home = () => {
+
+    const [pearlsList, setPearlsList] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+      useEffect(() => {
+        getPearls().then((PearlsData) => {
+            setPearlsList(PearlsData)
+            setIsLoading(false);
+        })
+      }, [])
+    
+    if (isLoading) return <p>Pearls are loading...</p> 
+
     return (
         <div>
             <div className="home-container">
@@ -11,18 +28,27 @@ const Home = () => {
                     </h4>
                 </div>
             </div>
-            <div className="pearl-container">
-                <div className='pearl-content'>
-                    <div className="orb">
-                        <div className="reflection"></div>
-                        <div className="inner-glow"></div>
-                        <div className="outer-shadow"></div>
-                        <div className="pearl-text">
-                            <h4>“I will be like the moon and learn to shine even when I am not whole.”</h4>
+
+            <section>
+            {pearlsList.map((pearl) => {
+                return (
+                    <div className="pearl-container">
+                        <div className='pearl-content'>
+                            <div className="orb">
+                                <div className="reflection"></div>
+                                <div className="inner-glow"></div>
+                                <div className="outer-shadow"></div>
+                                <div className="pearl-text">
+                                    <h4>{pearl.username}</h4>
+                                    <br />
+                                    <h4>{pearl.body}</h4>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
+                )
+            })}
+            </section>
         </div>
     )
 }
