7f7a11300ce437709e8fc32679d47065b0f92e43
Ikrambo92 <ikki2292@gmail.com>
2022-11-22 16:55:44 +0000

    updated styling

--- wordpearl/src/Components/Login.jsx
+++ wordpearl/src/Components/Login.jsx
@@ -18,57 +18,57 @@ const Login = () => {
     }
 
     const handleChangePassword = (event) => {
-       setPassword(event.target.value)
+        setPassword(event.target.value)
     }
 
     const handleClick = (event) => {
-      if(username === '' || password === ''){
-        alert('Both the username and password must be filled in')
-        return
-       }
-       event.preventDefault()
-       getOysterByUsername(username).then((response) => {
-       if(response.length !== 0 && password === response[0].password){
-        setIsLogginIn(true)
-        setUser(response[0])
-        setIsLogginIn(false)
-       } else {
-        alert('username or password is incorrect')
-       }
-       })
+        if (username === '' || password === '') {
+            alert('Both the username and password must be filled in')
+            return
+        }
+        event.preventDefault()
+        getOysterByUsername(username).then((response) => {
+            if (response.length !== 0 && password === response[0].password) {
+                setIsLogginIn(true)
+                setUser(response[0])
+                setIsLogginIn(false)
+            } else {
+                alert('username or password is incorrect')
+            }
+        })
     }
 
     const handleLogoutClick = () => {
         setUser({})
     }
 
-    if(isLogginIn) return <h2>Loggin in...</h2>
-    if(user.id === undefined){
-    return (
-        <div className="login-container">
-            <div className='creds'>
-                <h2 id='login' >Login</h2>
-                <h3>Username</h3>
-                <form id='form'>
-                <input type="text" name="username-input" id="uname" placeholder='John123' onChange={handleChangeUsername} />
-                <h3>Password</h3>
-                <input type="text" name="password-input" id="pname" placeholder='Password123' onChange={handleChangePassword}/>
-                <div className="button">
-                    <button className='button' onClick={handleClick} >Login</button>
-                </div>
-                <Link to='/signup' >Don't have an account? sign-up...</Link>
-                </form>
-            </div>
-        </div>
-    )
-    } else {
+    if (isLogginIn) return <h2>Loggin in...</h2>
+    if (user.id === undefined) {
         return (
-            <div>
-              <h2>You're signed in as {user.username}</h2>
-              <button onClick={handleLogoutClick}>Logout</button>
+            <div className="login-container">
+                <div className='creds'>
+                    <h2 id='login' >Login</h2>
+                    <h3>Username</h3>
+                    <form id='form'>
+                        <input type="text" name="username-input" id="uname" placeholder='John123' onChange={handleChangeUsername} />
+                        <h3>Password</h3>
+                        <input type="text" name="password-input" id="pname" placeholder='Password123' onChange={handleChangePassword} />
+                        <div className="button">
+                            <button className='button' onClick={handleClick} >Login</button>
+                        </div>
+                        <Link to='/signup' >Don't have an account? sign-up...</Link>
+                    </form>
+                </div>
             </div>
         )
-      }
+    } else {
+        return (
+            <div className='after-login'>
+                <h2>You're signed in as {user.username}</h2>
+                <button className='logout' onClick={handleLogoutClick}>Logout</button>
+            </div>
+        )
+    }
 }
 
 export default Login
--- wordpearl/src/Components/SingleOyster.jsx
+++ wordpearl/src/Components/SingleOyster.jsx
@@ -20,33 +20,35 @@ const SingleOyster = () => {
         })
     }, [user.username])
 
-    if(user.id === undefined){
-        return <h2>Please <Link to={'/Login'} >log in</Link></h2>
+    if (user.id === undefined) {
+        return <div className="logout-message">
+            <h2>Please <Link to={'/Login'} >log in</Link></h2>
+        </div>
     } else {
-    return (
-        <article>
-            <div className="card-container1">
-                <div className="card1">
-                    <div className="lines1"></div>
-                    <div className="imgBx">
-                        <img src={user.avatar_url} alt={user.username}></img>
-                    </div>
-                    <div className="content1">
-                        <div className="details1">
-                            <h2> Author: {user.username}</h2>
-                            <p> points: <span>{user.points}</span></p>
+        return (
+            <article>
+                <div className="card-container1">
+                    <div className="card1">
+                        <div className="lines1"></div>
+                        <div className="imgBx">
+                            <img src={user.avatar_url} alt={user.username}></img>
+                        </div>
+                        <div className="content1">
+                            <div className="details1">
+                                <h2> Author: {user.username}</h2>
+                                <p> points: <span>{user.points}</span></p>
 
+                            </div>
                         </div>
                     </div>
+                    {pearlsList.map((pearl) => {
+                        return <PearlCard key={pearl.pearl_id} pearl={pearl} />
+                    })}
                 </div>
-                {pearlsList.map((pearl) => {
-                    return <PearlCard key={pearl.pearl_id} pearl={pearl} />
-                })}
-            </div>
 
-        </article>
-    )
-  }
+            </article>
+        )
+    }
 }
 
 export default SingleOyster;
