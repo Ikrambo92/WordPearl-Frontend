cb2f582c538eef11a7e0e32e32934d07f71c842c
wystler <benjamin.rudd@live.com>
2022-11-23 12:19:00 +0000

    add delete button to singlepearl that redirects you to the logged in profile after successful delete

--- wordpearl/src/Components/SinglePearl.jsx
+++ wordpearl/src/Components/SinglePearl.jsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { getOysterByUsername, getPearlById, patchPearlById, putOyster } from '../Utils/apis';
+import { useContext } from 'react';
+import { UserContext } from '../Context/UserContext';
+import { getOysterByUsername, getPearlById, patchPearlById, putOyster, deletePearlById } from '../Utils/apis';
 import CommentsById from './Comments';
 import "./SinglePearl.css"
-
+import { useNavigate } from 'react-router-dom';
 const SinglePearl = () => {
-
+  const navigate = useNavigate()
+  const { user } = useContext(UserContext)
   const { id } = useParams();
   const [isLoading, setIsLoading] = useState(true);
   const [ pearl, setPearl ] = useState({})
@@ -14,19 +17,17 @@ const SinglePearl = () => {
   const [ voteChange, setVoteChange ] = useState(0)
   const [isvoteUp, setIsVoteUp] = useState(false)
   const [isvoteDown, setIsVoteDown] = useState(false)
-
-
+  const [deleting, setDeleting] = useState(false)
   useEffect(() => {
     getPearlById(id).then((res) => {
       setPearl(res)
       setVoteCount(res.votes)
       getOysterByUsername(res.username).then((res) => {
         setOyster(res[0])
-        setIsLoading(false)       
+        setIsLoading(false)
       })
     })
   },[id, voteCount])
-
   useEffect(() => {
     if (voteChange > 0){
       setIsVoteUp(true)
@@ -39,32 +40,35 @@ const SinglePearl = () => {
       setIsVoteDown(false)
     }
   }, [voteChange])
-
-
   const handleClickUp = () => {
     setVoteCount((CurrentVoteCount) => CurrentVoteCount + 1)
     setVoteChange((currentChange) => currentChange + 1)
     patchPearlById(pearl.id, voteCount + 1)
     putOyster(oyster.id, oyster.points + 1)
   }
-
   const handleClickDown = () => {
     setVoteCount((CurrentVoteCount) => CurrentVoteCount - 1)
     setVoteChange((currentChange) => currentChange - 1)
     patchPearlById(pearl.id, voteCount - 1)
     putOyster(oyster.id, oyster.points - 1)
   }
-
+  const handleDelete = (event) => {
+    setDeleting(true)
+    deletePearlById(event.target.value).then((response) => {
+      setDeleting(false)
+      navigate('/Oyster')
+    })
+  }
   if (isLoading) return (<h2>Loading pearl...</h2>)
-
-
       return (
       <main>
+        {deleting && <p>Deleting comment...</p>}
         <section id="singlepearl">
             <h2>{pearl.title}</h2>
                   <h3>Pearl: {pearl.body}</h3>
                   <h3>Author: {pearl.username}</h3>
                   <h4>Date: {pearl.created_at}</h4>
+                  {user.username !== pearl.username ? <></> : <button id="deleteButton" value={id} onClick={handleDelete}>Delete 🗑️</button>}
           <div>
             <button onClick={() => handleClickUp()} disabled={isvoteUp}>👍</button>
             <button onClick={() => handleClickDown()} disabled={isvoteDown}>👎</button>
@@ -75,6 +79,4 @@ const SinglePearl = () => {
       </main>
     );
 }
-
-
 export default SinglePearl;
